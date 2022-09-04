26245788d217abe955f4ab98871c7aa835a32398
Canion3D <80270614+canion3d@users.noreply.github.com>
2022-09-04 21:56:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -80,15 +80,15 @@ if option == 'Home':
 if option == 'Model Viewer':
     st.markdown("<h1 style='text-align: center; color: white;'>View Models with the Online Model Viewer</h1>" , unsafe_allow_html = True)
 
-st.components.v1.iframe("https://3dviewer.net", width=1024, height=768, scrolling=False)
+    st.components.v1.iframe("https://3dviewer.net", width=1024, height=768, scrolling=False)
 
 #online model viewer courtesy of MIT.
 
 if option == 'Slicer':
 
-st.markdown("<h1 style='text-align: center; color: white;'>Slice your models using the online slicer</h1>" , unsafe_allow_html = True)
+    st.markdown("<h1 style='text-align: center; color: white;'>Slice your models using the online slicer</h1>" , unsafe_allow_html = True)
 
-st.components.v1.iframe("https://icesl.loria.fr/webprinter/", width=1600, height=1600, scrolling=True)
+    st.components.v1.iframe("https://icesl.loria.fr/webprinter/", width=1600, height=1600, scrolling=True)
 
 #online slicer courtesy of Slicecrafter is powered by Emscripten.
 
