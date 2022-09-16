e21538353de7c0a6767822f72150afa6de67c85d
Canion3D <80270614+canion3d@users.noreply.github.com>
2022-09-16 13:31:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ if selected == "SLS & BinderJet Quote": ("")
 # 2. horizontal menu
 selected2 = option_menu("", ["Main", "Upload Model", "Slicer", "3DP Analytics", "Service Bureau Search", "LTZBLD Blockchain", "SLS & BinderJet Quote"],
         icons=['house', 'cloud-upload', 'menu-app', 'menu-app','menu-app', 'menu-app', 'menu-app'],
-        menu_icon="cast", default_index=0, orientation="vertical",styles={
+        menu_icon="cast", default_index=0, orientation="horizontal",styles={
         "container": {"padding": "0!important", "background-color": "#fafafa"},
         "icon": {"color": "orange", "font-size": "15px"},
         "nav-link": {"font-size": "15px", "text-align": "left", "margin":"10px", "--hover-color": "#eee"},
