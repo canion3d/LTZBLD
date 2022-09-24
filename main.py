6543dcbf401fcd9cf12c1fad2cfd5ffa05ff6cad
Canion3D <80270614+canion3d@users.noreply.github.com>
2022-09-24 18:43:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -270,15 +270,15 @@ if option == 'Service Bureau Connect':
 if option == 'Blockchain Service':
 	
 # Imports
-import os
-import requests
-from dotenv import load_dotenv
-load_dotenv()
-from bip44 import Wallet
-from web3 import Account
-from web3 import middleware
-from web3.gas_strategies.time_based import medium_gas_price_strategy
-from web3 import Web3
+   import os
+   import requests
+   from dotenv import load_dotenv
+   load_dotenv()
+   from bip44 import Wallet
+   from web3 import Account
+   from web3 import middleware
+   from web3.gas_strategies.time_based import medium_gas_price_strategy
+   from web3 import Web3
 
 w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
 
