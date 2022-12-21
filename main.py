100f00a466b88001ecea727ffe28cb25171a756c
canion3d <tyrone.canion@canion3d.com>
2022-12-21 21:39:02 +0100

    Update

--- main.py
+++ main.py
@@ -202,10 +202,7 @@ if option == '3DP Analytics':
         label = "Available 3D Printers",
         value = "100"
     )
-
-    st.line_chart(chart_data = pd.DataFrame(
-        np.random.randn(20,3),
-        columns=["Length","Width","Size"]))
+    pandas.read_xml(dataSources.xml)
 
 if option == 'Service Bureau Connect':
     st.markdown("<h1 style='text-align: center; color: white;'>Service Bureau Connect!</h1>" , unsafe_allow_html = True)
