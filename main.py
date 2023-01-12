efbca629b4c5b8aa09436b799e779508635d9395
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-01-12 20:26:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -246,7 +246,6 @@ if option == 'Blockchain Service':
    import requests
    from dotenv import load_dotenv
    load_dotenv()
-   from bip44 import Wallet
    from web3 import Account
    from web3 import middleware
    from web3.gas_strategies.time_based import medium_gas_price_strategy
