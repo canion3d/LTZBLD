9925fa279f2bb6439be718912473b6e98f4323f4
canion3d <tyrone.canion@canion3d.com>
2023-01-22 15:34:09 +0100

    Update

--- main.py
+++ main.py
@@ -20,52 +20,7 @@ st.set_page_config(
 	page_icon="tada"
      )
 
-# streamlit_app.py
-
-# Security
-#passlib,hashlib,bcrypt,scrypt
-import hashlib
-def make_hashes(password):
-	return hashlib.sha256(str.encode(password)).hexdigest()
-
-def check_hashes(password,hashed_text):
-	if make_hashes(password) == hashed_text:
-		return hashed_text
-	return False
-# DB Management
-import sqlite3 
-conn = sqlite3.connect('3DPASS.db')
-c = conn.cursor()
-# DB  Functions
-def create_usertable():
-	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
-
-
-def add_userdata(username,password):
-	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
-	conn.commit()
-
-def login_user(username,password):
-	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
-	data = c.fetchall()
-	return data
-
-
-def view_all_users():
-	c.execute('SELECT * FROM userstable')
-	data = c.fetchall()
-	return data
-
-
-
-def main():
-	"""LTZBLD login"""
-
-col1 , col2 , col3 = st.columns(3)
-
 with col1 :
-    st.write('LTZBLD')
-
 
 def img_to_html(param) :
     pass
