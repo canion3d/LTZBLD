27e0f691e6c9592ccb1d873c229483ea452f2053
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-02-20 20:14:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -137,7 +137,10 @@ with st.sidebar:
 
 if selected == "Main": ("")
 
-if selected ==  "Upload Model" : ("")
+if selected ==  "Upload Model":
+    st.markdown("<h1 style='text-align: center; color: white;'>View Models with the Online Model Viewer</h1>" , unsafe_allow_html = True)
+
+    st.components.v1.iframe("https://3dviewer.net", width=1024, height=768, scrolling=False)
 
 if selected == "Slicer": os.startfile("")
 
