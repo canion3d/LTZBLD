0db36a8cf3d0ca94144d5b4cdde0e7827ffe0a58
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-03-06 20:48:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def main():
 
 	st.title("LTZBLD")
 
-	st.image('Canion3D_original-logos_PNG.png', use_column_width=0)
+	st.video("https://www.youtube.com/watch?v=g29Xl8PlQ0M&t=39s")
 
 	st.markdown("<h1 style='text-align: center; color: black;'>LTZBLD - Let's Build!</h1>", unsafe_allow_html=True)
 
