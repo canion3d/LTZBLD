5116aa4dbdc31230c4b64b3ee2a0ee288b0ad9aa
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-03-20 21:00:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -213,18 +213,7 @@ newsapi = NewsApiClient(api_key='e05f54f819fb43b4b67385072ad1db10')
 top_headlines = newsapi.get_top_headlines(q='bitcoin',
                                           sources='bbc-news,the-verge',
                                           language='en',
-                                          )
-
-# /v2/everything
-all_articles = newsapi.get_everything(q='bitcoin',
-                                      sources='bbc-news,the-verge',
-                                      domains='bbc.co.uk,techcrunch.com',
-                                      from_param='2017-12-01',
-                                      to='2017-12-12',
-                                      language='en',
-                                      sort_by='relevancy',
-                                      page=2)
-
+					  )
 # /v2/top-headlines/sources
 sources = newsapi.get_sources()
 
