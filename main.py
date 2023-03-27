d9d69224886ca947ff721d0b325bd0b22f7f3a13
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-03-27 17:32:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -208,19 +208,71 @@ def page3():
     st.markdown("# 3D Printing News 🎉")
     st.sidebar.markdown("# 3D Printing News 🎉")
 	
-import requests
+# Preinstalled packages
+import requests # 2.24.0
 
-url = "https://api.newscatcherapi.com/v2/search"
+# Default packages
+import json
+import time
 
-querystring = {"q":"\"Elon Musk\"","lang":"en","sort_by":"relevancy","page":"1"}
+# URL of our News API
+base_url = "https://api.newscatcherapi.com/v2/search"
 
-headers = {
-    "x-api-key": "MrdTeq8_09jPXZuHbxYOmpXGH2ZxgDAr9sILyzyJ9iQ"
-    }
+# Your API key
+X_API_KEY = 'MrdTeq8_09jPXZuHbxYOmpXGH2ZxgDAr9sILyzyJ9iQ'
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+# Define your desired parameters
+params = {
+    "q": "\"Elon Musk\"",
+    "from": "13 days ago",
+    "countries": "IN,GB",
+    "page_size": 100,
+    "page": 1
+}
 
-print(response.text)	
+# Put your API key to headers in order to be authorized to perform a call
+headers = {"x-api-key": X_API_KEY}
+
+
+# Variable to store all found news articles
+all_news_articles = {}
+
+# Ensure that we start from page 1
+params['page'] = 1
+
+# Infinite loop which ends when all articles are extracted
+while True:
+
+    # Wait for 1 second between each call
+    time.sleep(1)
+
+    # GET Call
+    response = requests.get(base_url, headers=headers, params=params)
+    results = json.loads(response.text.encode())
+    if response.status_code == 200:
+        print(f'Done for page number => {params["page"]}/{results["total_pages"]}')
+
+
+        # Storing all found articles
+        if not all_news_articles:
+            all_news_articles = results
+        else:
+            all_news_articles['articles'].extend(results['articles'])
+
+        # Ensuring to cover all pages by incrementing "page" value at each iteration
+        params['page'] += 1
+        if params['page'] > results['total_pages']:
+            print("All articles have been extracted")
+            break
+        else:
+            print(f'Proceed extracting page number => {params["page"]}')
+    else:
+        print(results)
+        print(f'ERROR: API call failed for page number => {params["page"]}')
+        break
+
+print(f'Number of extracted articles => {str(len(all_news_articles))}')
+
 
 page_names_to_funcs = {
     "Home Page": main_page,
