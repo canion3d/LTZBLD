6ec1f331862dd089a88b7ee744dd635cb9fd6ee4
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-03-27 11:28:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,9 @@ selected2 = option_menu("", ["Main", "Upload Model", "Slicer", "3DP Analytics", 
     }
 )
 
+st.markdown("""<a href="https://shareasale.com/r.cfm?b=2299301&u=3574798&m=139161&urllink=&afftrack=">Get 42% off Mole 3D Scanner. Shop Now!</a>
+", unsafe_allow_html=True)
+
 st.markdown("""
 <a href="https://click.linksynergy.com/fs-bin/click?id=8WC05bHq4DI&offerid=1207190.332&subid=0&type=4"><IMG border="0"   alt="Newegg" src="https://ad.linksynergy.com/fs-bin/show?id=8WC05bHq4DI&bids=1207190.332&subid=0&type=4&gridnum=1"></a>
 """, unsafe_allow_html=True)
