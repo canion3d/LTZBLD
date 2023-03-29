e4f764d4e99917f7f19eeea7c44e99529c29edef
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-03-29 20:28:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -129,13 +129,13 @@ st.sidebar.video("3DPaaSgood.mp4")
 def get_fvalue(val):
 	feature_dict = {"No": 1, "Yes": 2}
 	for key, value in feature_dict.items():
-if val == key:
-	return value
+		if val == key:
+		return value
 
 def get_value(val, my_dict):
 	for key, value in my_dict.items():
-if val == key:
-	return value
+		if val == key:
+		return value
 
 st.markdown(
 "Canion3D Mission Statement: To be a provider across all industries of only the best 3D Printing Products/Services, "
