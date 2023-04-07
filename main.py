b03317de94c5859261604522a197d81da06aff1c
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-04-07 17:00:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ selected2 = option_menu("", ["Main", "Upload Model", "Slicer", "3DP Analytics", 
         icons=['house', 'cloud-upload', 'menu-app', 'menu-app','menu-app', 'menu-app', 'menu-app'],
         menu_icon="cast", default_index=0, orientation="horizontal",styles={
         "container": {"padding": "0!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "15px"},
-        "nav-link": {"font-size": "15px", "text-align": "left", "margin":"10px", "--hover-color": "#eee"},
+        "icon": {"color": "black", "font-size": "11px"},
+        "nav-link": {"font-size": "11px", "text-align": "left", "margin":"10px", "--hover-color": "#eee"},
         "nav-link-selected": {"background-color": "red"},
     }
 )
