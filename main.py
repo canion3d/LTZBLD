1cfe244c498f40414b89137ce9114b91cd59b05a
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-04-12 22:33:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -255,45 +255,163 @@ for article in data["articles"]:
     st.write("---")
 
 def page4():
-    st.markdown("# 3D Model Search  ❄️")
-    st.sidebar.markdown("# 3D Model Search ❄️")
+    st.markdown("# Order a 3D Print  ❄️")
+    st.sidebar.markdown("# Order a 3D Print ❄️")
     
-    st.markdown("<h1 style='text-align: center; color: white;'>Search for 3D Models - Courtesy of Cults3D!</h1>" , unsafe_allow_html = True)
+    st.markdown("<h1 style='text-align: center; color: white;'>Get a quote and Order a 3D Print!</h1>" , unsafe_allow_html = True)
 	
+import selectors
+import streamlit as st
+import pandas as pd
+import base64
+import numpy as np
+import streamlit.components.v1 as components
+import hmac
+import hashlib
+import datetime
+import json
 import requests
+import altair as al
+import sqlite3
+from streamlit_option_menu import option_menu
+from pip._internal import main
+from streamlit_option_menu import option_menu
+
 import streamlit as st
 
-# Define the API endpoint and parameters
-url = 'https://cults3d.com/graphql'
-query = '''
-    {
-        creations(limit: 1) {
-            name
-            url
-            creator {
-                any
-            }
-        }
+st.markdown("""
+<a href="https://click.linksynergy.com/fs-bin/click?id=8WC05bHq4DI&offerid=1207190.332&subid=0&type=4"><IMG border="0"   alt="Newegg" src="https://ad.linksynergy.com/fs-bin/show?id=8WC05bHq4DI&bids=1207190.332&subid=0&type=4&gridnum=1"></a>
+""", unsafe_allow_html=True)
+
+# Security
+#passlib,hashlib,bcrypt,scrypt
+import hashlib
+def make_hashes(password):
+	return hashlib.sha256(str.encode(password)).hexdigest()
+
+def check_hashes(password,hashed_text):
+	if make_hashes(password) == hashed_text:
+		return hashed_text
+	return False
+# DB Management
+import sqlite3
+conn = sqlite3.connect('3DPASS.db')
+c = conn.cursor()
+# DB  Functions
+def create_usertable():
+	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
+
+
+def add_userdata(username,password):
+	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+	conn.commit()
+
+def login_user(username,password):
+	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
+	data = c.fetchall()
+	return data
+
+
+def view_all_users():
+	c.execute('SELECT * FROM userstable')
+	data = c.fetchall()
+	return data
+
+
+
+def main():
+	"""LTZBLD login"""
+
+	st.title("LTZBLD")
+
+	menu = ["Home","Login","SignUp"]
+	choice = st.sidebar.selectbox("Menu",menu)
+
+	if choice == "Home":
+		st.subheader("Home")
+
+	elif choice == "Login":
+		st.subheader("Login Section")
+
+		username = st.sidebar.text_input("User Name")
+		password = st.sidebar.text_input("Password",type='password')
+		if st.sidebar.checkbox("Login"):
+			# if password == '12345':
+			create_usertable()
+			hashed_pswd = make_hashes(password)
+
+			result = login_user(username,check_hashes(password,hashed_pswd))
+			if result:
+
+				st.success("Logged In as {}".format(username))
+
+				task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
+				if task == "Add Post":
+					st.subheader("Add Your Post")
+
+				elif task == "Analytics":
+					st.subheader("Analytics")
+				elif task == "Profiles":
+					st.subheader("User Profiles")
+					user_result = view_all_users()
+					clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
+					st.dataframe(clean_db)
+			else:
+				st.warning("Incorrect Username/Password")
+
+	elif choice == "SignUp":
+		st.subheader("Create New Account")
+		new_user = st.text_input("Username")
+		new_password = st.text_input("Password",type='password')
+
+		if st.button("Signup"):
+			create_usertable()
+			add_userdata(new_user,make_hashes(new_password))
+			st.success("You have successfully created a valid Account")
+			st.info("Go to Login Menu to login")
+
+
+
+if __name__ == '__main__':
+	main()
+
+primaryColor="#F63366"
+backgroundColor="#DDDDDD"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
+
+col1, col2, col3 = st.columns(3)
+
+st.sidebar.header("Apps and Features")
+
+# 1. as sidebar menu
+with st.sidebar:
+    selected = option_menu("", ["Upload Model & get Quote", "Slicer", "3DP Analytics", "Service Bureau Search", "LTZBLD Blockchain", "SLS & BinderJet Quote"],
+        icons=['house', 'cloud-upload', 'menu-app', 'menu-app','menu-app', 'menu-app', 'menu-app'],
+        menu_icon="cast", default_index=0, orientation="vertical",styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "15px"},
+        "nav-link": {"font-size": "15px", "text-align": "left", "margin":"10px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "red"},
     }
-'''
-auth = ('Canion3DInc', ' LJdIFYtM2RXPxz582uezRhpgg')
+)
 
-# Send a POST request to the API endpoint
-response = requests.post(url, json={'query': query}, auth=auth)
 
-# Parse the JSON response
-if response.ok:
-    data = response.json()['data']
-    st.write(data)
-else:
-    st.error(f'Request failed with status code {response.status_code}.')
+if selected == "Upload Model & get Quote":
+    st.markdown("<h1 style='text-align: center; color: white;'>View Models with the Online Model Viewer</h1>",
+                unsafe_allow_html=True)
 
+    st.components.v1.iframe("https://3dviewer.net", width=1024, height=768, scrolling=False)
+
+    st.components.v1.iframe("https://www.treatstock.com/order-upload/widget?posUid=fixedPs&psId=3567", width=1024,
+                            height=768, scrolling=False)
 
 page_names_to_funcs = {
     "Home Page": main_page,
     "Model Viewer": page2,
     "Technology News": page3,
-    "3D Model Search": page4,
+    "Get a quote and Order a 3D Print": page4,
 }
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
