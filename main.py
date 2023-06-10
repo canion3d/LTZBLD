b4319ecdcdebc7e50bba42355767f12a475dc303
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-06-10 20:40:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ def main():
 	choice = st.sidebar.selectbox("Menu",menu)
 
 if choice == "Home":
-		st.subheader("Home")
+	st.subheader("Home")
 
 elif choice == "Login":
 	st.subheader("Login Section")
