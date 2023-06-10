d5cbc35d78829f9326d55f7f138b7ec98eca29cb
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-06-10 20:36:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -136,6 +136,27 @@ def main():
 	menu = ["Home","Login","SignUp"]
 	choice = st.sidebar.selectbox("Menu",menu)
 
+if choice == "Home":
+		st.subheader("Home")
+
+	elif choice == "Login":
+		st.subheader("Login Section")
+
+		username = st.sidebar.text_input("User Name")
+		password = st.sidebar.text_input("Password",type='password')
+		if st.sidebar.checkbox("Login"):
+			# if password == '12345':
+			create_usertable()
+			hashed_pswd = make_hashes(password)
+
+			result = login_user(username,check_hashes(password,hashed_pswd))
+			if result:
+
+				st.success("Logged In as {}".format(username))
+
+				task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
+				if task == "Add Post":
+					st.subheader("Add Your Post")
 
 				elif task == "Analytics":
 					st.subheader("Analytics")
