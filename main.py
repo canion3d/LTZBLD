73324db5c4fdc29f926c2f6d377f23c534cf8dab
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-06-15 20:10:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,12 @@ st.markdown("""
 <a href="https://click.linksynergy.com/fs-bin/click?id=8WC05bHq4DI&offerid=1207190.332&subid=0&type=4"><IMG border="0"   alt="Newegg" src="https://ad.linksynergy.com/fs-bin/show?id=8WC05bHq4DI&bids=1207190.332&subid=0&type=4&gridnum=1"></a>
 """, unsafe_allow_html=True)
 
+st.markdown("""
+<div class="shrsl_ShareASale_productShowCaseTarget_50616"></div>
+<script type="text/javascript" src="https://showcase.shareasale.com/shareASale_liveWidget_loader.js?dt=06072023044506"></script>
+<script type="text/javascript">shrsl_ShareASale_liveWid_Init(50616, 3574798, 'shrsl_ShareASale_liveWid_wideSkyScraper_populate');</script>
+""")
+
 # Security
 #passlib,hashlib,bcrypt,scrypt
 import hashlib
