cb294551172da3936c561eb54b92161340cd39a9
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-07-23 12:58:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,22 @@ st.markdown("""
 <a href="https://click.linksynergy.com/fs-bin/click?id=8WC05bHq4DI&offerid=1207190.332&subid=0&type=4"><IMG border="0"   alt="Newegg" src="https://ad.linksynergy.com/fs-bin/show?id=8WC05bHq4DI&bids=1207190.332&subid=0&type=4&gridnum=1"></a>
 """, unsafe_allow_html=True)
 
+# Add divider line
+st.markdown("---")
+
+st.markdown("<h2 style='text-align: center; color: white;'>Select a dashboard to get started: </h2>", unsafe_allow_html=True)
+
+# List available page files
+page_files = os.listdir("Pages")
+page_files = [file for file in page_files if file.endswith(".py")]
+
+# Add a menu item to select pages
+selected_pages = st.multiselect("Select Trade Cipher Features", page_files)
+
+# Execute the selected pages
+for selected_page in selected_pages:
+    exec(open(f"Pages/{selected_page}").read())
+
 # Security
 #passlib,hashlib,bcrypt,scrypt
 import hashlib
