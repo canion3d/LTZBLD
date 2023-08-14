8b8fb1d7e9f4b499150ea5c5816469c9eda2274c
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-08-14 18:30:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import sqlite3
 from streamlit_option_menu import option_menu
 from pip._internal import main
 from streamlit_option_menu import option_menu
-from st_paywall import auth
+import st_paywall
 
 add_auth(required=True)
 
