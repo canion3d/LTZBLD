0a7348c878f4ab105965a2af0893f09f55c11393
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-09-04 21:34:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ selected_pages = st.multiselect("Select Trade Cipher Features", page_files)
 
 #Execute the selected pages
 for selected_page in selected_pages:
-exec(open(f"Pages/{selected_page}").read())
+	exec(open(f"Pages/{selected_page}").read())
 
 #Add divider line
 st.markdown("---")
